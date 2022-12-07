d7bb167b8522dc36b24fca581f7dc28862dc5c69
demi <demi@watcha.com>
2022-12-07 21:22:10 +0900

    commit 1.1.8 :1

--- app/src/main/java/com/example/giphy_update/util/viewExtension.kt
+++ app/src/main/java/com/example/giphy_update/util/viewExtension.kt
@@ -24,7 +24,7 @@ import java.util.concurrent.TimeUnit
 fun clicks(view: View, ms: Long): Observable<View> = Observable.create<View> { emitter ->
     view.setOnClickListener {
         emitter.onNext(it)
-///1.1.6
+///1.1.8 :1
     }
 }.throttleFirst(ms, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
 
